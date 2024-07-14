659e07c0d58712ea79dfd933278ad63f40e64a29
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-07-14 10:54:01 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"gopportunities/config"
 	"gopportunities/router"
 )
 
-func main() {
+var (
+	logger *config.Logger
+)
 
+func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	//Initialize routes
 	router.Initialize()
 }
