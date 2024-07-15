1a0a5d9017ad1e3110179951d9844a195a03cf18
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-07-15 08:54:35 -0300

    feat: add update handler logic

--- handler/request.go
+++ handler/request.go
@@ -41,3 +41,23 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+// UpdateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	//if any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+
+	//if none of the fields were provided, return falsy
+	return fmt.Errorf("at least one value field must be provided")
+}
