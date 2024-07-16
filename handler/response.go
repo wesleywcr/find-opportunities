ce571da9462b314c215e35be3acacbf88e6d79cd
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-07-16 08:43:21 -0300

    docs: add documentation for delete opening

--- handler/response.go
+++ handler/response.go
@@ -33,20 +33,7 @@ type CreateOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
-
 type DeleteOpeningResponse struct {
-	Message string                  `json:"message"`
-	Data    schemas.OpeningResponse `json:"data"`
-}
-type ShowOpeningResponse struct {
-	Message string                  `json:"message"`
-	Data    schemas.OpeningResponse `json:"data"`
-}
-type ListOpeningsResponse struct {
-	Message string                    `json:"message"`
-	Data    []schemas.OpeningResponse `json:"data"`
-}
-type UpdateOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
