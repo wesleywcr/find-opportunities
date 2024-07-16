3ebd9b0d72a815db0da3a66467cf1dad062ac477
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-07-16 08:44:30 -0300

    docs: add documentation for show opening

--- handler/response.go
+++ handler/response.go
@@ -37,3 +37,7 @@ type DeleteOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
